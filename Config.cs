6a3b4df841275f0ec7afbb591b9e683c12876d77
Kevin Ledford Richard <134469571+l1ghthousekeeper@users.noreply.github.com>
2026-05-08 06:48:04 -0400

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SnykTest
+{
+    public class Config
+    {
+        public string GetDbPassword()
+        {
+            string myDbPassword = "P@ssw0rd123!"; 
+            return myDbPassword;
+        }
+    }
+}
